1911e63cab8693cc1ad61b6c1b1019c5987858aa
Nikita <you@example.com>
2024-01-08 08:25:44 +0300

    app.js fix

--- app/app.js
+++ app/app.js
@@ -3,5 +3,5 @@ const app = express()
 const os = require("os");
 const hostname = os.hostname();
 
-app.get('/', (req, res) => res.send('Test website. Server running at ${hostname}'))
+app.get('/', (req, res) => res.send(`Test website. Server running at ${hostname}`))
 app.listen(3000, () => console.log('Server ready'))
